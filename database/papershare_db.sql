1d51021f3a7ffa0168002fc45d1d10a849bfb70a
Ανδρέας Μανίτσας <andrewmanitsas@gmail.com>
2017-06-04 19:24:16 +0300

    refreshed database

--- database/papershare_db.sql
+++ database/papershare_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Φιλοξενητής: 127.0.0.1
--- Χρόνος δημιουργίας: 04 Ιουν 2017 στις 04:13:10
+-- Χρόνος δημιουργίας: 04 Ιουν 2017 στις 18:23:35
 -- Έκδοση διακομιστή: 10.1.21-MariaDB
 -- Έκδοση PHP: 7.1.2
 
@@ -43,7 +43,8 @@ CREATE TABLE `accounts` (
 
 INSERT INTO `accounts` (`user_id`, `username`, `password`, `name`, `surname`, `email`, `age`, `sex`) VALUES
 (2, 'andrewmanitsas', '123', 'Andrew', 'Manitsas', 'st0709@icte.uowm.gr', 22, 'male'),
-(3, 'mariakasi86', '123', 'Maria', 'Kasiara', 'mariakasiara@gmail.com', 31, 'female');
+(3, 'mariakasi86', '123', 'Maria', 'Kasiara', 'mariakasiara@gmail.com', 31, 'female'),
+(4, 'parlapipas', '123', 'John', 'Xatz', '123@gmail.com', 99, 'other');
 
 -- --------------------------------------------------------
 
@@ -57,8 +58,19 @@ CREATE TABLE `books` (
   `title` varchar(1000) COLLATE utf8_unicode_520_ci NOT NULL,
   `author` varchar(1000) COLLATE utf8_unicode_520_ci NOT NULL,
   `type` varchar(1000) COLLATE utf8_unicode_520_ci NOT NULL,
-  `price` int(100) NOT NULL
+  `price` int(100) NOT NULL,
+  `user_id` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_520_ci;
+
+--
+-- Άδειασμα δεδομένων του πίνακα `books`
+--
+
+INSERT INTO `books` (`book_id`, `isbn`, `title`, `author`, `type`, `price`, `user_id`) VALUES
+(1, 852456, 'AAABBBCCC', 'abc', 'book', 20, 2),
+(2, 123456, 'TTTTTTTTT', 'aabb', 'RRRRR', 10, 4),
+(3, 789852, 'EEEEEEEE', 'aart', 'PPP', 30, 3),
+(4, 0, 'Perfect Zero', 'Giannis Hatzoglou', 'Garbage', -10000, 2);
 
 --
 -- Ευρετήρια για άχρηστους πίνακες
@@ -84,12 +96,12 @@ ALTER TABLE `books`
 -- AUTO_INCREMENT για πίνακα `accounts`
 --
 ALTER TABLE `accounts`
-  MODIFY `user_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `user_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT για πίνακα `books`
 --
 ALTER TABLE `books`
-  MODIFY `book_id` int(5) NOT NULL AUTO_INCREMENT;
+  MODIFY `book_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
